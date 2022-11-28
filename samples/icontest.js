7842f5454563cd97a70b3359cbcfe6fe484f525c
ag143 <ag14341@gmail.com>
2022-11-28 10:32:25 +0530

    Update icontest.js

--- samples/icontest.js
+++ samples/icontest.js
@@ -1,7 +1,7 @@
 // a selection of free icons from https://icomoon.io/
 // the following paths are all pure SVG path strings that GoJS parses as Geometry strings.
 
-var icons = {
+var icontest = {
   "home":
   "M32 18.451l-16-12.42-16 12.42v-5.064l16-12.42 16 12.42zM28 18v12h-8v-8h-8v8h-8v-12l12-9z",
 
